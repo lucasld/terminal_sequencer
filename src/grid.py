72de24df4aea1e11f8f317d3b75f56cf1fc107d2
lucasld <lucas.ld@hermes-link.de>
2022-07-22 09:35:36 +0200

    added colors and 'left side prototype'

--- src/grid.py
+++ src/grid.py
@@ -13,9 +13,18 @@ class Grid:
         self.loop_end_chr = config['loop_end_chr']
         self.sound_manager = sound_manager
 
+        self.begin_grid_index = 9
+
         # init console
         self.console = curses.initscr()
         curses.start_color()
+        curses.use_default_colors()
+        curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_YELLOW)
+        curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_WHITE, 235)
+        curses.init_pair(4, -1, curses.COLOR_WHITE)
+        #for i in range(0, curses.COLORS):
+        #    curses.init_pair(i + 1, i, -1)
         self.HEIGHT, self.WIDTH = self.console.getmaxyx()
         self.Y, self.X = 0, 0  # y and x position of the selector
 
@@ -30,21 +39,35 @@ class Grid:
     def init_grid(self):
         self.grid = np.empty((self.HEIGHT-1, self.WIDTH-1, 5), dtype='U3')
         self.grid[True] = self.empty_space_chr
+        self.grid[1::2, :, 0] = ' '
+        self.grid[::2, self.begin_grid_index - 1, 0] = '#'
+        self.grid[1::2, self.begin_grid_index - 1, 0] = '#'
+    
+
+    def set_runner(self):
+        # check if we are in the right row
+        if not self.Y % 2:
+            x = max(self.begin_grid_index, self.X)
+            self.grid[self.Y, x, 1] = self.runner_chr
+            self.grid[self.Y, x, 3] = '-1'
+            self.grid[self.Y, x, 4] = '-1'
 
     
     def move_coord(self, y: int, x: int, dir: tuple) -> tuple:
         ny, nx = y + dir[0], x + dir[1]
+        if nx == self.begin_grid_index - 1:
+            nx += dir[1]
         if 0 <= nx < self.grid.shape[1] and 0 <= ny < self.grid.shape[0]:
             return ny, nx, True
         return y, x, False
 
 
-    def _get_start_pos(self, y, x, last_begin_loop):
+    def _get_start_pos(self, y, x, last_begin_loop) -> tuple:
         begin_loop_pos = np.argwhere(self.grid[y, :, 0] == self.loop_begin_chr)
         begin_loop_pos = [e for e in begin_loop_pos if e < x]
         if not len(begin_loop_pos):
             # there is no begin loop set, go back to line beginning instead
-            return 0, -1
+            return self.begin_grid_index, -1
         last_begin_loop += 1
         if last_begin_loop >= len(begin_loop_pos):
             last_begin_loop = 0
@@ -87,12 +110,6 @@ class Grid:
                             # check if note is hit and play it
                             if self.grid[row, nx, 0] in self.sound_manager.sounds.keys():
                                 self.sound_manager.play(self.grid[row, nx, 0])
-                                #pygame.mixer.Channel(self.current_channel).play(self.sounds[self.grid[row, nx, 2]])
-                                #self.current_channel += 1
-                                #if self.current_channel >= self.channel_number - 1:
-                                #   self.current_channel = 0
-
-
 
     
     def draw_grid(self):
@@ -100,9 +117,15 @@ class Grid:
         for y in range(self.grid.shape[0]):
             for x in range(self.grid.shape[1]):
                 # highlight and play when selector position
-                color = curses.A_STANDOUT if y==self.Y and x==self.X else curses.COLOR_BLACK
+                #color = curses.color_pair(4) if y==self.Y and x==self.X else curses.COLOR_BLACK
                 if self.grid[y, x, 1] == self.empty_space_chr:
                     character = self.grid[y, x, 0]
+                    if character == '#':
+                        color = curses.color_pair(1)
+                    elif (y//2)%2:
+                        color = curses.color_pair(2)
+                    else:
+                        color = curses.color_pair(3)
                 else:
                     # highlight note when runner is on it
                     if self.grid[y, x, 0] in self.sound_manager.sounds.keys() and self.grid[y, x, 1] == self.runner_chr: 
@@ -110,5 +133,7 @@ class Grid:
                         color = curses.A_STANDOUT
                     else:
                         character = self.grid[y, x, 1]
+                if y==self.Y and x==self.X:
+                    color = curses.color_pair(4)
                 self.console.addch(y, x, character, color)
         self.console.refresh()
--- src/keyboard.py
+++ src/keyboard.py
@@ -4,7 +4,6 @@ from pynput import keyboard
 class Keyboard:
 
     def __init__(self, config, grid_manager):
-        self.set_runner = config['set_runner']
         self.loop_begin = config['loop_begin']
         self.loop_end = config['loop_end']
 
@@ -18,21 +17,28 @@ class Keyboard:
     def on_press(self, key):
         if type(key) == keyboard.Key:
             if key == key.down:
-                self.grid.Y, self.grid.X, _ = self.grid.move_coord(self.grid.Y, self.grid.X, (1, 0))
+                self.grid.Y, self.grid.X, _ = self.grid.move_coord(self.grid.Y,
+                                                                   self.grid.X,
+                                                                   (1, 0))
             elif key == key.up:
-                self.grid.Y, self.grid.X, _ = self.grid.move_coord(self.grid.Y, self.grid.X, (-1, 0))
+                self.grid.Y, self.grid.X, _ = self.grid.move_coord(self.grid.Y,
+                                                                   self.grid.X,
+                                                                   (-1, 0))
             elif key == key.left:
-                self.grid.Y, self.grid.X, _ = self.grid.move_coord(self.grid.Y, self.grid.X, (0, -1))
+                self.grid.Y, self.grid.X, _ = self.grid.move_coord(self.grid.Y,
+                                                                   self.grid.X,
+                                                                   (0, -1))
             elif key == key.right:
-                self.grid.Y, self.grid.X, _ = self.grid.move_coord(self.grid.Y, self.grid.X, (0, 1))
+                self.grid.Y, self.grid.X, _ = self.grid.move_coord(self.grid.Y,
+                                                                   self.grid.X,
+                                                                   (0, 1))
             elif key == key.backspace:
-                if self.grid.grid[self.grid.Y, self.grid.X, 0] == self.grid.empty_space_chr:
-                    self.grid.Y, self.grid.X, _ = self.grid.move_coord(self.grid.Y, self.grid.X, (0, -1))
                 self.grid.grid[self.grid.Y, self.grid.X, :] = self.grid.empty_space_chr
-            elif key == eval(self.set_runner):
-                self.grid.grid[self.grid.Y, self.grid.X, 1] = self.grid.runner_chr
-                self.grid.grid[self.grid.Y, self.grid.X, 3] = '-1'
-                self.grid.grid[self.grid.Y, self.grid.X, 4] = '-1'
+                self.grid.Y, self.grid.X, _ = self.grid.move_coord(self.grid.Y,
+                                                                   self.grid.X,
+                                                                   (0, -1))
+            elif key == key.enter:
+                self.grid.set_runner()
         elif type(key) == keyboard.KeyCode:
             if key.char == self.loop_begin:
                 self.grid.grid[self.grid.Y, self.grid.X, 0] = self.grid.loop_begin_chr
